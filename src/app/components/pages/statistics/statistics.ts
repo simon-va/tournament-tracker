5076dd893a52d9600a9432a680d311d73d5715b5
Simon Vahlbrock <vahlbrock.s@com-online.com>
2025-10-02 14:37:09 +0200

    updated history items

--- src/app/components/pages/statistics/statistics.ts
+++ src/app/components/pages/statistics/statistics.ts
@@ -5,12 +5,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { RunningGameConfig } from '../../../services/game.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics.html',
   styleUrl: './statistics.scss',
-  imports: [MatExpansionModule, MatButtonModule, MatCardModule, MatIconModule],
+  imports: [
+    MatExpansionModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
 })
 export class StatisticsComponent {
   private readonly gameHistoryService = inject(GameHistoryService);
@@ -27,8 +34,17 @@ export class StatisticsComponent {
     this.gameHistoryService.removeGameFromHistory(id);
   }
 
-  protected getWinnerNames(gameItem: RunningGameConfig): string {
-    const teamId = this.getWinnerTeam(gameItem);
+  protected getWinnerNames(
+    gameItem: RunningGameConfig,
+    teamId?: number,
+  ): string {
+    if (!teamId) {
+      teamId = this.getWinnerTeam(gameItem);
+
+      if (teamId === 0) {
+        return 'Unentschieden';
+      }
+    }
 
     const team =
       gameItem?.[teamId === 1 ? 'team1Players' : 'team2Players'] ?? [];
@@ -38,6 +54,21 @@ export class StatisticsComponent {
     }
 
     return team.join(', ');
+  }
+
+  protected formatDate(date: Date): string {
+    const dateFormatter = new Intl.DateTimeFormat('de-DE', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
+
+    const timeFormatter = new Intl.DateTimeFormat('de-DE', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+
+    return `${dateFormatter.format(date)}, ${timeFormatter.format(date)} Uhr`;
   }
 
   private getWinnerTeam(gameItem: RunningGameConfig): number {
